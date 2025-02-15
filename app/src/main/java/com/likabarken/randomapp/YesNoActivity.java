eecec9c73236348bf82076be71adf67b384963a0
userlika <likaforjob@gmail.com>
2025-02-16 01:29:46 +0300

    Added russian language and refactores yesno activity

--- app/src/main/java/com/likabarken/randomapp/YesNoActivity.java
+++ app/src/main/java/com/likabarken/randomapp/YesNoActivity.java
@@ -2,6 +2,7 @@ package com.likabarken.randomapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Random;
 
 public class YesNoActivity extends AppCompatActivity {
+    TextView textViewRandomYesNo;
+    Button buttonAnswer;
+    private String yes, no, maybe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +29,30 @@ public class YesNoActivity extends AppCompatActivity {
             return insets;
         });
 
-        String[] options = {"Yes", "No", "Maybe"};
+        initViews();
+
+        String[] options = {yes, no, maybe};
         Random randomIndex = new Random();
         int randomIntRange = randomIndex.nextInt(3);
 
-        TextView textViewRandomYesNo = findViewById(R.id.textViewRandomYesNo);
-
         textViewRandomYesNo.setText(options[randomIntRange]);
 
-        textViewRandomYesNo.setOnClickListener(new View.OnClickListener() {
+        buttonAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int randomIntRange = randomIndex.nextInt(3);
                 textViewRandomYesNo.setText(options[randomIntRange]);
+                textViewRandomYesNo.setVisibility(View.VISIBLE);
             }
         });
 
+    }
+
+    private void initViews() {
+        textViewRandomYesNo = findViewById(R.id.textViewRandomYesNo);
+        buttonAnswer = findViewById(R.id.buttonAnswer);
+        yes = getString(R.string.yes);
+        no = getString(R.string.no);
+        maybe = getString(R.string.maybe);
     }
 }
